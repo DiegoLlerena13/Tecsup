3f2d3151a136724711ea013d5233c5637d020a10
DiegoLlerena13 <dllerenat@unsa.edu.pe>
2024-07-29 22:07:14 -0500

    eliminando static

--- tiendaOnline/tiendaOnline/urls.py
+++ tiendaOnline/tiendaOnline/urls.py
@@ -17,14 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tienda import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home')
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
