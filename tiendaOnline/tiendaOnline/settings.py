1c69b0f11e2e8b6d4038e84d3f597705d7c2f43f
DiegoLlerena13 <dllerenat@unsa.edu.pe>
2024-08-05 16:18:52 -0500

    vercel

--- tiendaOnline/tiendaOnline/settings.py
+++ tiendaOnline/tiendaOnline/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8u=zjm*_p_5qil_xe6e@@cq=u+)^=8s=hi1u9+27xg3&%46f0_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
